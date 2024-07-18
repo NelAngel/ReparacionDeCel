f2b7494d0ad15b4134af47f35a6a5127fbefd506
Angel Chuchullo Chacca <achuchullo@unsa.edu.pe>
2024-07-18 09:46:31 -0500

    Se arreglo el js

--- scripts.js
+++ scripts.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-document.addEventListener('DOMContentLoaded', () => {
-    const menuToggle = document.getElementById('menuToggle');
-    const sidebar = document.getElementById('sidebar');
-
-    menuToggle.addEventListener('click', () => {
-        sidebar.classList.toggle('active');
-    });
+document.getElementById('menuToggle').addEventListener('click', function() {
+    document.getElementById('sidebar').classList.toggle('active');
 });
-=======
-document.addEventListener('DOMContentLoaded', () => {
-    const menuToggle = document.getElementById('menuToggle');
-    const sidebar = document.getElementById('sidebar');
-
-    menuToggle.addEventListener('click', () => {
-        sidebar.classList.toggle('active');
-    });
-});
->>>>>>> 1be5e2294f9877930ea5119a4f5915b668ecc200

1be5e2294f9877930ea5119a4f5915b668ecc200
NelAngel <132708628+NelAngel@users.noreply.github.com>
2024-07-18 00:03:12 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.getElementById('menuToggle');
+    const sidebar = document.getElementById('sidebar');
+
+    menuToggle.addEventListener('click', () => {
+        sidebar.classList.toggle('active');
+    });
+});
